47eaf89f7b50b80dbeae62394e206e77653c21cc
DEMKOM58 <demkom58@gmail.com>
2019-07-07 23:23:41 +0300

    Support of 1.14 - 1.14.3 maybe and newer...

---
+++ src/main/java/com/demkom58/divinedrop/versions/V14R1/V14R1.java
@@ -0,0 +1,64 @@
+package com.demkom58.divinedrop.versions.V14R1;
+
+import com.demkom58.divinedrop.lang.Language;
+import com.demkom58.divinedrop.versions.V12R1.V12Listener;
+import com.demkom58.divinedrop.versions.V13R1.V13LangParser;
+import com.demkom58.divinedrop.versions.Version;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class V14R1 implements Version {
+    public static final String VERSION = "1.14";
+    public static final String PATH = "minecraft/lang/%s.json";
+
+    @Override
+    public String getI18NDisplayName(@Nullable ItemStack item) {
+        if(item == null) return null;
+        return getName(item);
+    }
+
+    @NotNull
+    @Override
+    public String getLangPath(@NotNull final String locale) {
+        return String.format(PATH, locale.toLowerCase());
+    }
+
+    @NotNull
+    @Override
+    public Map<String, String> parseLang(@NotNull InputStream inputStream) throws IOException {
+        return V13LangParser.parseLang(inputStream);
+    }
+
+    @NotNull
+    @Override
+    public String name() {
+        return VERSION;
+    }
+
+    @NotNull
+    @Override
+    public Listener getListener() {
+        return new V12Listener();
+    }
+
+    private String getName(ItemStack bItemStack) {
+        final net.minecraft.server.v1_14_R1.ItemStack itemStack = org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack.asNMSCopy(bItemStack);
+        final net.minecraft.server.v1_14_R1.NBTTagCompound nbtTagCompound = itemStack.b("display");
+
+        if (nbtTagCompound != null) {
+            if (nbtTagCompound.hasKeyOfType("Name", 8))
+                return nbtTagCompound.getString("Name");
+            if (nbtTagCompound.hasKeyOfType("LocName", 8))
+                return Language.getInstance().getLocName(nbtTagCompound.getString("LocName"));
+        }
+
+        return Language.getInstance().getLocName(itemStack.getItem().getName()).trim();
+    }
+
+}
--- src/main/java/com/demkom58/divinedrop/versions/VersionUtil.java
+++ src/main/java/com/demkom58/divinedrop/versions/VersionUtil.java
@@ -6,6 +6,7 @@ import com.demkom58.divinedrop.versions.V11R1.V11R1;
 import com.demkom58.divinedrop.versions.V12R1.V12R1;
 import com.demkom58.divinedrop.versions.V13R1.V13R1;
 import com.demkom58.divinedrop.versions.V13R2.V13R2;
+import com.demkom58.divinedrop.versions.V14R1.V14R1;
 import com.demkom58.divinedrop.versions.V8R3.V8R3;
 import com.demkom58.divinedrop.versions.V9R1.V9R1;
 import com.demkom58.divinedrop.versions.V9R2.V9R2;
@@ -48,6 +49,7 @@ public class VersionUtil {
         if ("v1_12_R1".equals(ver)) return new V12R1();
         if ("v1_13_R1".equals(ver)) return new V13R1();
         if ("v1_13_R2".equals(ver)) return new V13R2();
+        if ("v1_14_R1".equals(ver)) return new V14R1();
         return null;
     }
 
